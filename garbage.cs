b1d3880362b8008c140251f235650d123a6db9af
oleksandra <104913146+leksi-s@users.noreply.github.com>
2022-05-05 13:32:10 +0300

    Add files via upload

---
+++ garbage.cs
@@ -0,0 +1,21 @@
+﻿class Garbage
+{
+    public string Type { get; set; }
+    public int Size { get; set; }
+    public string Name { get; set; }
+
+    public Garbage(string type, int size, string name)
+    {
+        Type = type;
+        Name = name;
+        Size = size;
+    }
+
+    public override string ToString()
+    {
+        return $"{Type}   size: {Size}  name: {Name}";
+    }
+
+
+}
+
